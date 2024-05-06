d4f693b8d2c9e626c672d3e646af97f3c7fc025c
Pham Trung Nguyen <phamtrungnguyen2288@gmail.com>
2024-05-06 22:14:04 +0700

    Update cart reduxt

--- redux/cart/CartReducers.js
+++ redux/cart/CartReducers.js
@@ -1,67 +1,67 @@
-import {
-    ADD_TO_CART, REMOVE_FROM_CART, INCREMENT_QUANTITY,DECREMENT_QUANTITY,CLEAN_CART
-  } from "../../constants/constants.js";
+// import {
+//     ADD_TO_CART, REMOVE_FROM_CART, INCREMENT_QUANTITY,DECREMENT_QUANTITY,CLEAN_CART
+//   } from "../../constants/constants.js";
   
   
   
 
-const initialState = {
-    cart: [],
-  };
+// const initialState = {
+//     cart: [],
+//   };
   
-  export const cartReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case ADD_TO_CART:
-        const itemPresent = state.cart.find((item) => item.id === action.payload.id);
-        if (itemPresent) {
-          return {
-            ...state,
-            cart: state.cart.map((item) =>
-              item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
-            ),
-          };
-        } else {
-          return {
-            ...state,
-            cart: [...state.cart, { ...action.payload, quantity: 1 }],
-          };
-        }
-      case REMOVE_FROM_CART:
-        return {
-          ...state,
-          cart: state.cart.filter((item) => item.id !== action.payload.id),
-        };
-      case INCREMENT_QUANTITY:
-        return {
-          ...state,
-          cart: state.cart.map((item) =>
-            item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
-          ),
-        };
-      case DECREMENT_QUANTITY:
-        const item = state.cart.find((item) => item.id === action.payload.id);
-        if (item.quantity === 1) {
-          return {
-            ...state,
-            cart: state.cart.filter((item) => item.id !== action.payload.id),
-          };
-        } else {
-          return {
-            ...state,
-            cart: state.cart.map((item) =>
-              item.id === action.payload.id ? { ...item, quantity: item.quantity - 1 } : item
-            ),
-          };
-        }
-      case CLEAN_CART:
-        return {
-          ...state,
-          cart: [],
-        };
-      default:
-        return state;
-    }
-  };
+//   export const cartReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//       case ADD_TO_CART:
+//         const itemPresent = state.cart.find((item) => item.id === action.payload.id);
+//         if (itemPresent) {
+//           return {
+//             ...state,
+//             cart: state.cart.map((item) =>
+//               item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
+//             ),
+//           };
+//         } else {
+//           return {
+//             ...state,
+//             cart: [...state.cart, { ...action.payload, quantity: 1 }],
+//           };
+//         }
+//       case REMOVE_FROM_CART:
+//         return {
+//           ...state,
+//           cart: state.cart.filter((item) => item.id !== action.payload.id),
+//         };
+//       case INCREMENT_QUANTITY:
+//         return {
+//           ...state,
+//           cart: state.cart.map((item) =>
+//             item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
+//           ),
+//         };
+//       case DECREMENT_QUANTITY:
+//         const item = state.cart.find((item) => item.id === action.payload.id);
+//         if (item.quantity === 1) {
+//           return {
+//             ...state,
+//             cart: state.cart.filter((item) => item.id !== action.payload.id),
+//           };
+//         } else {
+//           return {
+//             ...state,
+//             cart: state.cart.map((item) =>
+//               item.id === action.payload.id ? { ...item, quantity: item.quantity - 1 } : item
+//             ),
+//           };
+//         }
+//       case CLEAN_CART:
+//         return {
+//           ...state,
+//           cart: [],
+//         };
+//       default:
+//         return state;
+//     }
+//   };
   
-  export default cartReducer;
+//   export default cartReducer;
   
--- redux/cart/CartSaga.js
+++ redux/cart/CartSaga.js
@@ -1,36 +1,36 @@
 
-import {
-    ADD_TO_CART, REMOVE_FROM_CART, INCREMENT_QUANTITY,DECREMENT_QUANTITY,CLEAN_CART
-  } from "../../constants/constants.js";
+// import {
+//     ADD_TO_CART, REMOVE_FROM_CART, INCREMENT_QUANTITY,DECREMENT_QUANTITY,CLEAN_CART
+//   } from "../../constants/constants.js";
   
 
 
-import { takeLatest, put } from "redux-saga/effects";
+// import { takeLatest, put } from "redux-saga/effects";
 
-function* addToCartSaga(action) {
-  // Put your saga logic here
-}
+// function* addToCartSaga(action) {
+//   // Put your saga logic here
+// }
 
-function* removeFromCartSaga(action) {
-  // Put your saga logic here
-}
+// function* removeFromCartSaga(action) {
+//   // Put your saga logic here
+// }
 
-function* incrementQuantitySaga(action) {
-  // Put your saga logic here
-}
+// function* incrementQuantitySaga(action) {
+//   // Put your saga logic here
+// }
 
-function* decrementQuantitySaga(action) {
-  // Put your saga logic here
-}
+// function* decrementQuantitySaga(action) {
+//   // Put your saga logic here
+// }
 
-function* cleanCartSaga(action) {
-  // Put your saga logic here
-}
+// function* cleanCartSaga(action) {
+//   // Put your saga logic here
+// }
 
-export function* watchCartActions() {
-  yield takeLatest(ADD_TO_CART, addToCartSaga);
-  yield takeLatest(REMOVE_FROM_CART, removeFromCartSaga);
-  yield takeLatest(INCREMENT_QUANTITY, incrementQuantitySaga);
-  yield takeLatest(DECREMENT_QUANTITY, decrementQuantitySaga);
-  yield takeLatest(CLEAN_CART, cleanCartSaga);
-}
+// export function* watchCartActions() {
+//   yield takeLatest(ADD_TO_CART, addToCartSaga);
+//   yield takeLatest(REMOVE_FROM_CART, removeFromCartSaga);
+//   yield takeLatest(INCREMENT_QUANTITY, incrementQuantitySaga);
+//   yield takeLatest(DECREMENT_QUANTITY, decrementQuantitySaga);
+//   yield takeLatest(CLEAN_CART, cleanCartSaga);
+// }
